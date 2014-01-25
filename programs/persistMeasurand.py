3e9987ab517509ade8d6535a951c18b661563c8d
Matthias Gabriel <gabm@hrz.tu-chemnitz.de>
2014-01-25 17:56:01 +0100

    added protocol handler

--- programs/persistMeasurand.py
+++ programs/persistMeasurand.py
@@ -1,10 +1,11 @@
-#!/mnt/opt/usr/bin/python
+#!/usr/bin/python
 
 import optparse
 import time
-import struct
 import socket
 import MeasurandDAO
+from protocol_backend import *
+
 
 BUFFER_SIZE = 1024
 
@@ -13,13 +14,18 @@ def persistMeasurand(hostname, portnumber, sensorid, locationid, unit):
 	val = getValue(hostname, portnumber, sensorid)
 	
 	# persist value to db
-	persistValue(val, unit, locationid, sensorid)
+	# persistValue(val, unit, locationid, sensorid)
 
 def getValue(hostname, portnumber, sensorid):
+	# startup socket
 	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	sock.connect((hostname, portnumber))
 	
-	sock.send(struct.pack("B", sensorid))
+	# let the protocol backend generate our packet
+	backend = protocol_backend()
+	sock.send(backend.generatePacket(Commands.Sensor_Readout, sensorid))
+	   
+	# receive the result
 	receivedValue = ''
 	while (receivedValue == ''):
 		receivedValue = sock.recv(BUFFER_SIZE)
@@ -27,23 +33,20 @@ def getValue(hostname, portnumber, sensorid):
 	# close socket
 	sock.close()
 		
-	value = struct.unpack("H", receivedValue[1:])[0]
-	value = value/2.
-	if value > 100:
-		value = -256.0 + value
-	
+	# let the backend decode the received value
+	value = backend.decodePacket(receivedValue)
+
+	print value
 	return value
 	
 def persistValue(value, unit, locationid, sensorid):
-	
-	print value
 	
 	daoMeasurand = MeasurandDAO.MeasurandDAO()
 	daoMeasurand.persistMeasurand(value, unit, locationid, sensorid)
 	daoMeasurand.closeHandle()
 
 parser = optparse.OptionParser("usage: %prog [options] arg1 arg2")
-parser.add_option("-H", "--host", dest="hostname", default="77.64.132.34", type="string", help="weatherstation lan ip")
+parser.add_option("-H", "--host", dest="hostname", default="192.168.0.50", type="string", help="weatherstation lan ip")
 parser.add_option("-p", "--port", dest="portnum", default=2069, type="int", help="port number to run from weatherstation")
 parser.add_option("-s", "--sensor-id", dest="sensorid", default=1, type="int", help="sensor id")
 parser.add_option("-l", "--location-id", dest="locationid", default=1, type="int", help="location id")
